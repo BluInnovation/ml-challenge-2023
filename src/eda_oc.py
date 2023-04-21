0aa6f0e5e2daf976265e3d7616666a733ada0f3a
oskrgab <ogcg12@gmail.com>
2023-04-21 10:39:39 -0500

    Got good model_2 with 4.93 RMSE.

--- src/eda_oc.py
+++ src/eda_oc.py
@@ -9,6 +9,7 @@ from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 from statsmodels.tsa.seasonal import seasonal_decompose
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 import pickle
+from statsmodels.tsa.statespace.sarimax import SARIMAX
 # %% Load data
 data_folder = Path("data")
 data_train = pd.read_csv(data_folder / "illinois_basing_train.csv")
@@ -58,12 +59,19 @@ plt.tight_layout()
 plt.show()
 # %% Remove outliers using IQ range for each column
 for col in data_train_2.columns:
-    data_train_2[col] = remove_outliers_iqr(data_train_2[col], multiplier=5)
+    data_train_2[col] = remove_outliers_iqr(data_train_2[col], multiplier=300)
 # %% Interpolate missing values for each column
 for col in data_train_2.columns:
     data_train_2[col].interpolate(method="linear", inplace=True)
 # %% Drop nan values
 data_train_2.dropna(inplace=True)
+#%% Plot the new inj diff column
+sns.set()
+sns.set_style("whitegrid")
+sns.lineplot(x="SampleTimeUTC", y="inj_diff", data=data_train_2, linewidth=0.5)
+plt.gcf().autofmt_xdate()
+plt.ylim(-50, 50)
+plt.show()
 # %% Plot lineplot of all the columns in the data_train_2 in a different axes
 data_train_2.plot(linewidth=0.5, subplots=True, figsize=(10, 10))
 plt.tight_layout()
@@ -93,7 +101,8 @@ plt.gcf().autofmt_xdate()
 plt.tight_layout()
 plt.show()
 #%% Select the values from 2012 onwards
-data_train_cut = data_train_2.loc["2012-01-01":]
+data_train_cut = data_train_2.loc["2011-01-01":]
+
 # %% Visualize the time series
 pm.tsdisplay(data_train_cut["inj_diff"], lag_max=100, figsize=(15, 10))
 plt.show()
@@ -104,7 +113,7 @@ plot_pacf(data_train_cut["inj_diff"], lags=50, ax=ax_4[1])
 plt.tight_layout()
 plt.show()
 # %% Decompose the time series
-decomposition = seasonal_decompose(data_train_cut["inj_diff"], period=24 * 7 * 4)
+decomposition = seasonal_decompose(data_train_cut["inj_diff"], period=24 * 7)
 fig_5 = decomposition.plot()
 fig_5.set_size_inches(15, 10)
 plt.tight_layout()
@@ -113,8 +122,11 @@ plt.show()
 D = pm.arima.utils.nsdiffs(
     data_train_cut["inj_diff"], m=24, max_D=24, test="ch"
 )
-# %% Train with SARIMAX using pmdarima
-model = pm.auto_arima(
+#%% Load original test data
+y_test = pd.read_csv("data/illinois_basing_test_original.csv")
+y_test = y_test["inj_diff_Calculated"]
+#%% Train auto ARIMA model
+model_auto = pm.auto_arima(
     data_train_cut["inj_diff"],
     X=data_train_cut.drop("inj_diff", axis=1),
     start_p=0,
@@ -130,30 +142,40 @@ model = pm.auto_arima(
     suppress_warnings=True,
     stepwise=True,
 )
+#%% Train SARIMAX model
+model = pm.ARIMA(
+    order=(2, 2, 1),
+    seasonal_order=(0, 1, 1, 24),
+    enforce_stationarity=False,
+    enforce_invertibility=False,
+)
+#%%
+
+model.fit(data_train_cut["inj_diff"], data_train_cut.drop("inj_diff", axis=1))
+#%% Save model to pickle file
+with open("model.pkl", "wb") as f:
+    pickle.dump(model, f)
+#%% Load model from pickle file
+with open("model.pkl", "rb") as f:
+    model = pickle.load(f)
 #%% Print model summary
+model = model_auto
 print(model.summary())
 #%% Plot model diagnostics
 model.plot_diagnostics(figsize=(15, 10))
 plt.tight_layout()
 plt.show()
-#%% Predict with SARIMAX
+#%% Calculate the predictions
 pred = model.predict(n_periods=len(data_test_2), X=data_test_2)
-#%% Load original test data
-y_test = pd.read_csv("data/illinois_basing_test_original.csv")
-y_test = y_test["inj_diff_Calculated"]
-# %% Plot the predictions for comparison
+#%% Plot the predictions for comparison
 plt.plot(data_test_2.index, pred, label="prediction")
 plt.plot(data_test_2.index, y_test, label="actual")
 plt.gcf().autofmt_xdate()
-plt.ylim(3, -3)
+# plt.ylim(3, -3)
 plt.legend()
 plt.show()
 #%% Calculate the mean absolute error
-mae = mean_absolute_error(y_test[1:], pred[1:])
-print(f"Mean absolute error: {mae}")
-#%% Calculate the mean squared error
-mse = mean_squared_error(y_test[1:], pred[1:])
-print(f"Mean squared error: {mse}")
+rmse = np.sqrt(mean_squared_error(y_test[1:], pred[1:]))
 #%% Export pred as csv with column name "inj_diff"
 pred = pd.DataFrame(pred, columns=["inj_diff"])
 pred.to_csv("data/pred.csv", index=False)
@@ -162,6 +184,3 @@ df_y_test = pd.DataFrame(y_test)
 df_y_test.index = data_test_2.index
 df_y_test.columns = ["inj_diff"]
 df_y_test["pred"] = pred.values
-#%% Save model to pickle file
-with open("model.pkl", "wb") as f:
-    pickle.dump(model, f)
